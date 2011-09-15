b0f2363f2384bda327d12877e65b426dfb09a164
Amber Yust <amber.yust@gmail.com>
2011-09-15 00:51:24 -0700

    Make memcached integration optional.
    If the python 'memcache' module is not available for import, the
    Cache class will simply behave as if everything is a cache miss -
    Cache.call() will transparently call the specified function and
    return its results, and all of the other Cache class functions
    will do nothing and return None.

--- util/cache.py
+++ util/cache.py
@@ -1,14 +1,28 @@
 import hashlib
 import json
-import memcache
+try:
+	import memcache
+	_hush_pyflakes = (memcache,)
+	del _hush_pyflakes
+except ImportError:
+	memcache = None
 
 class Cache(object):
-	"""Wrapper around a singleton memcache client."""
+	"""Wrapper around a singleton memcache client.
 
-	client = memcache.Client(['127.0.0.1:11211'], debug=0)
+	Note: If the 'memcache' library is not available,
+	this wrapper will do nothing - call() will transparently
+	always call the provided function, and everything else
+	will simply return None.
+	"""
+
+	client = memcache and memcache.Client(['127.0.0.1:11211'], debug=0)
 
 	@classmethod
 	def call(cls, func, *args, **kwargs):
+		if not cls.client:
+			return func(*args, **kwargs)
+
 		call_dump = json.dumps([func.__module__, func.__name__, args, kwargs])
 		memcache_key = 'pluss--%s' % hashlib.md5(call_dump).hexdigest()
 		result = cls.client.get(memcache_key)
@@ -19,20 +33,20 @@ class Cache(object):
 
 	@classmethod
 	def get(cls, *args, **kwargs):
-		return cls.client.get(*args, **kwargs)
+		return cls.client and cls.client.get(*args, **kwargs)
 
 	@classmethod
 	def set(cls, *args, **kwargs):
-		return cls.client.set(*args, **kwargs)
+		return cls.client and cls.client.set(*args, **kwargs)
 
 	@classmethod
 	def delete(cls, *args, **kwargs):
-		return cls.client.delete(*args, **kwargs)
+		return cls.client and cls.client.delete(*args, **kwargs)
 
 	@classmethod
 	def incr(cls, *args, **kwargs):
-		return cls.client.incr(*args, **kwargs)
+		return cls.client and cls.client.incr(*args, **kwargs)
 
 	@classmethod
 	def decr(cls, *args, **kwargs):
-		return cls.client.decr(*args, **kwargs)
+		return cls.client and cls.client.decr(*args, **kwargs)
