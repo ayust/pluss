8ef8a21f38e34924a28d32f9bfc43f33c4c190b5
Amber Yust <amber.yust@gmail.com>
2013-12-10 00:19:39 -0800

    Add production error logging

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 """pluss, a feed proxy for G+"""
+import logging
 
 from pluss.app import app
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter(
+            "%(asctime)s [%(process)d] [%(levelname)s] %(pathname)s:%(lineno)d %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        ))
+        app.logger.addHandler(handler)
+        app.logger.setLevel(logging.WARNING)
 
 if __name__ == '__main__':
     app.run(host='pluss.aiiane.com', port=54321, debug=True)
